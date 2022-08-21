bc0df8b2dc179e3ec7013c870a9dc4c0771f6ac0
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-21 23:24:52 +0300

    Инициализация проекта

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module dev.kalenchukov.string.regexp
+{
+	requires org.jetbrains.annotations;
+
+	exports dev.kalenchukov.string.regexp.parsing;
+	exports dev.kalenchukov.string.regexp.validation;
+}
