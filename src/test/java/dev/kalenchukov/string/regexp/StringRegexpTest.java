4072360718176d2df1f3ddb89623bc3fc3129f6d
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2023-02-06 00:28:05 +0300

    Изменена версия требуемого модуля junit до 5.8.2

--- src/test/java/dev/kalenchukov/string/regexp/StringRegexpTest.java
+++ src/test/java/dev/kalenchukov/string/regexp/StringRegexpTest.java
@@ -24,9 +24,9 @@
 
 package dev.kalenchukov.string.regexp;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringRegexpTest
 {
--- src/test/java/dev/kalenchukov/string/regexp/resources/RegexpTest.java
+++ src/test/java/dev/kalenchukov/string/regexp/resources/RegexpTest.java
@@ -24,12 +24,12 @@
 
 package dev.kalenchukov.string.regexp.resources;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RegexpTest
 {
